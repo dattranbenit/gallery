5e9d518cae46fdad7a2a00145432a133370b41d9
Your Name <dat@Dats-MacBook-Pro.local>
2022-03-10 10:09:34 +0700

    init

---
+++ src/comps/Carousel.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import ImageGallery from 'react-image-gallery';
+import "react-image-gallery/styles/css/image-gallery.css";
+import useFirestore from "../hooks/useFirestore";
+
+export default function Carousel() {
+  const { docs } = useFirestore('images');
+  let [images, setImages] = React.useState()
+  React.useEffect(() => {
+    if (docs) {
+      setImages(docs.map((doc) => {
+        return ({
+          original: doc.url,
+          thumbnail: doc.url,
+        })
+      }))
+    }
+  }, [docs])
+  return (
+    <>
+      {images && (
+        <ImageGallery
+          slideDuration={300}
+          slideInterval={2000}
+          items={images}
+          thumbnailPosition={"left"}
+          showBullets={true}
+        />
+      )}
+    </>
+  )
+}
